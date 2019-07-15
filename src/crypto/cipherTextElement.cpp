608f5526ca3db90221d1a8715112c94605c4d6e3
Felix Bilstein <fxb@cocacoding.com>
2019-07-15 14:33:57 +0200

    added prototype for ciphertext block list

---
+++ src/crypto/cipherTextElement.cpp
@@ -0,0 +1,1 @@
+#include "cipherTextElement.h"
---
+++ src/crypto/cipherTextElement.h
@@ -0,0 +1,20 @@
+#include "../commontypes.h"
+#include <pcapplusplus/SSLLayer.h>
+
+#ifndef CIPHERTEXTELEMENT_H
+#define CIPHERTEXTELEMENT_H
+
+/**
+ * @todo write docs
+ */
+class CipherTextElement
+{
+public:
+    std::string cipherSuite;
+    pcpp::SSLVersion sslVersion;
+    int length;
+    bool isClientBlock;
+    pcapfs::Bytes cipherBlock;
+};
+
+#endif // CIPHERTEXTELEMENT_H
