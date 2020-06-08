20ca88418f1288261c864a058f0ad44285a7e7ab
Martin Lambertz <martin.lambertz@fkie.fraunhofer.de>
2020-06-08 16:38:50 +0200

    Remove CentOS 6 specific stuff

--- scripts/dependencies/install-pcap-plus-plus.sh
+++ scripts/dependencies/install-pcap-plus-plus.sh
@@ -12,11 +12,6 @@ clone_or_update_git_repo "${URL}" "${pkgdir}"
 
 cd "${pkgdir}"
 
-distro_release="$(lsb_release -rs)"
-if [ "$(lsb_release -is)" = 'CentOS' -a "${distro_release%%.*}" = '6' ]; then
-    sed -i 's/\(PCAPPP_LIBS.*\)/\1 -lrt/'  mk/PcapPlusPlus.mk.linux
-fi
-
 yes 'no' | ./configure-linux.sh --install-dir "${PREFIX}"
 make all install
 
