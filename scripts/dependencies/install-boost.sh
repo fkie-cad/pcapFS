5b1804ad8fb170cda42ec4ab7e55b4068d913b6e
Martin Lambertz <martin.lambertz@fkie.fraunhofer.de>
2020-06-29 09:56:14 +0200

    Update install-boost.sh script
     - Update Boost version to 1.69
     - Cache downloaded sources to avoid Boost download limits

--- scripts/dependencies/install-boost.sh
+++ scripts/dependencies/install-boost.sh
@@ -5,12 +5,19 @@ SAVED_PWD="$(pwd -P)"
 HERE=$(dirname $(readlink -e $0))
 source "${HERE}/install-helpers.sh"
 
-URL='https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2'
+BOOST_MINOR_VERSION='69'
+BOOST_VERSION="1_${BOOST_MINOR_VERSION}_0"
+
+URL="https://dl.bintray.com/boostorg/release/1.${BOOST_MINOR_VERSION}.0/source/boost_${BOOST_VERSION}.tar.bz2"
 BOOST_LIBS='filesystem,iostreams,log,system,serialization,program_options'
 
 cd "${LOCAL_REPO_PATH}"
-wget "${URL}" -O- | tar -xjf-
-cd boost_1_64_0
+#wget "${URL}" -O- | tar -xjf-
+if [ ! -f "boost_${BOOST_VERSION}.tar.bz2" ]; then
+  wget "${URL}" -O "${DOWNLOADS}/boost_${BOOST_VERSION}.tar.bz2"
+fi
+tar -xjf "${DOWNLOADS}/boost_${BOOST_VERSION}.tar.bz2"
+cd "boost_${BOOST_VERSION}"
 ./bootstrap.sh --with-libraries="${BOOST_LIBS}" --prefix=${PREFIX}
 ./b2 install
 
