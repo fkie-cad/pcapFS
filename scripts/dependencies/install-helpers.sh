bbd570ee15f42201e078e7ba1471d0c21a372d7c
Martin Lambertz <martin.lambertz@fkie.fraunhofer.de>
2020-07-02 09:15:26 +0200

    Pin PcapPlusPlus to specific commit
    Currently used commit is 14a418ed4f9b72a832877dc8330e01259f617bf3.

--- scripts/dependencies/install-helpers.sh
+++ scripts/dependencies/install-helpers.sh
@@ -10,9 +10,21 @@ mkdir -p "${DOWNLOADS}"
 function clone_or_update_git_repo() {
     url="$1"
     pkgdir="$2"
+    commit=''
+
+    if [ $# -gt 2 ]; then
+        commit="$3"
+    fi
     if [ ! -d "${pkgdir}" ]; then
         git clone ${url} "${pkgdir}" --quiet
     else
-        cd "${pkgdir}" && git pull --quiet && cd ..
+        cd "${pkgdir}" || return
+        default_branch="$(git branch -a | sed -nE 's;.*origin/HEAD.*origin/(.*);\1;p')"
+        git checkout "${default_branch}"
+        git pull --quiet
+        cd ..
+    fi
+    if [ -n "${commit}" ]; then
+        cd "${pkgdir}" && git checkout "${commit}" && cd ..
     fi
 }
--- scripts/dependencies/install-pcap-plus-plus.sh
+++ scripts/dependencies/install-pcap-plus-plus.sh
@@ -6,9 +6,10 @@ HERE=$(dirname $(readlink -e $0))
 source "${HERE}/install-helpers.sh"
 
 URL='https://github.com/seladb/PcapPlusPlus.git'
+COMMIT='14a418ed4f9b72a832877dc8330e01259f617bf3'
 
 pkgdir="${LOCAL_REPO_PATH}/PcapPlusPlus"
-clone_or_update_git_repo "${URL}" "${pkgdir}"
+clone_or_update_git_repo "${URL}" "${pkgdir}" "${COMMIT}"
 
 cd "${pkgdir}"
 
