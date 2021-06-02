ba4274685e5acdc73a0f08b8f977e76943d77d1e
Felix Bilstein <fxb@cocacoding.com>
2021-06-02 12:11:19 +0200

    current stage

--- src/virtualfiles/ssl.cpp
+++ src/virtualfiles/ssl.cpp
@@ -1063,11 +1063,16 @@ size_t pcapfs::SslFile::read(uint64_t startOffset, size_t length, const Index &i
     }
 
     if (write_me_to_file.size() > 0) {
+
+    	LOG_TRACE << "offset: " << offset << " startOffset: " << startOffset <<
+    			" length: " << length << "result_size: " << write_me_to_file.size();
+
 		memset(buf, 0, length);
 		memcpy(buf, (const char*) write_me_to_file.data() + startOffset, length);
 
 		LOG_TRACE << "file writer done!" << std::endl;
-		LOG_TRACE << "offset: " << offset << " startOffset: " << startOffset << " length: " << length;
+    	LOG_TRACE << "offset: " << offset << " startOffset: " << startOffset <<
+    			" length: " << length << "result_size: " << write_me_to_file.size();
     }
 
     if (startOffset + length < filesizeRaw) {
