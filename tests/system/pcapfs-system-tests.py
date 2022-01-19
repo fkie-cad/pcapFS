740a2cc38c5ee9314f0034f04c9671a5a3be0d02
Felix Bilstein <fxb@cocacoding.com>
2022-01-20 00:45:04 +0100

    added unit tests for readRaw and readProcessed for ciphertext and plaintext testing

--- tests/system/pcapfs-system-tests.py
+++ tests/system/pcapfs-system-tests.py
@@ -4,6 +4,7 @@ import random
 import string
 import subprocess
 import tempfile
+import binascii
 from contextlib import contextmanager
 
 import pytest
@@ -58,6 +59,32 @@ class TestSsl:
     def test_with_single_ssl_key_file(self, test_pcap, expected_files_with_ssl):
         with mount_pcap(test_pcap, params=['-k', '{here}/keyfiles/ssl.key'.format(here=HERE)]) as mountpoint:
             assert get_file_list(mountpoint) == expected_files_with_ssl
+
+
+class TestSslFileReadRaw:
+
+    def test_read_raw_ssl_rc4_full_appdata(self, test_pcap, content_tls_appdata_all_cipher):
+        with mount_pcap(test_pcap) as mountpoint:
+            files = get_file_list(mountpoint)
+            assert('ssl/0-1838_SSL' in files)
+            with open(os.path.join(mountpoint, 'ssl/0-1838_SSL'), 'rb') as f:
+                content = f.read()
+                data = binascii.hexlify(content)
+                hexdata = str(data, 'UTF-8')
+                assert(hexdata == content_tls_appdata_all_cipher)
+
+
+class TestSslFileReadProcessed:
+
+    def test_read_processed_ssl_rc4_full_appdata(self, test_pcap, content_tls_appdata_all_plain):
+        with mount_pcap(test_pcap, params=['-k', '{here}/keyfiles/ssl.key'.format(here=HERE)]) as mountpoint:
+            files = get_file_list(mountpoint)
+            assert('ssl/0-1838_SSL' in files)
+            with open(os.path.join(mountpoint, 'ssl/0-1838_SSL'), 'rb') as f:
+                content = f.read()
+                data = binascii.hexlify(content)
+                hexdata = str(data, 'UTF-8')
+                assert(hexdata == content_tls_appdata_all_plain)
 
 
 class TestConfigFile:
@@ -154,3 +181,73 @@ def expected_files_with_ssl(expected_files):
     expected_files.remove('ssl/0-1838_SSL')
     expected_files.append('http/0-279')
     return sorted(expected_files)
+
+
+@pytest.fixture
+def content_tls_appdata_all_plain():
+    content_tls_appdata_all_plain = "474554202f20485454502f312e310a484f53543a206c6f63616c686f7374" \
+                                    "0a0a485454502f312e3120323030204f4b0d0a5365727665723a206e6769" \
+                                    "6e782f312e31302e3320285562756e7475290d0a446174653a205475652c" \
+                                    "2030342053657020323031382031343a34363a343820474d540d0a436f6e" \
+                                    "74656e742d547970653a20746578742f68746d6c0d0a436f6e74656e742d" \
+                                    "4c656e6774683a203631320d0a4c6173742d4d6f6469666965643a205475" \
+                                    "652c2030342053657020323031382031333a30363a313020474d540d0a43" \
+                                    "6f6e6e656374696f6e3a206b6565702d616c6976650d0a455461673a2022" \
+                                    "35623865383334322d323634220d0a4163636570742d52616e6765733a20" \
+                                    "62797465730d0a0d0a3c21444f43545950452068746d6c3e0a3c68746d6c" \
+                                    "3e0a3c686561643e0a3c7469746c653e57656c636f6d6520746f206e6769" \
+                                    "6e78213c2f7469746c653e0a3c7374796c653e0a20202020626f6479207b" \
+                                    "0a202020202020202077696474683a203335656d3b0a2020202020202020" \
+                                    "6d617267696e3a2030206175746f3b0a2020202020202020666f6e742d66" \
+                                    "616d696c793a205461686f6d612c2056657264616e612c20417269616c2c" \
+                                    "2073616e732d73657269663b0a202020207d0a3c2f7374796c653e0a3c2f" \
+                                    "686561643e0a3c626f64793e0a3c68313e57656c636f6d6520746f206e67" \
+                                    "696e78213c2f68313e0a3c703e496620796f752073656520746869732070" \
+                                    "6167652c20746865206e67696e7820776562207365727665722069732073" \
+                                    "75636365737366756c6c7920696e7374616c6c656420616e640a776f726b" \
+                                    "696e672e204675727468657220636f6e66696775726174696f6e20697320" \
+                                    "72657175697265642e3c2f703e0a0a3c703e466f72206f6e6c696e652064" \
+                                    "6f63756d656e746174696f6e20616e6420737570706f727420706c656173" \
+                                    "6520726566657220746f0a3c6120687265663d22687474703a2f2f6e6769" \
+                                    "6e782e6f72672f223e6e67696e782e6f72673c2f613e2e3c62722f3e0a43" \
+                                    "6f6d6d65726369616c20737570706f727420697320617661696c61626c65" \
+                                    "2061740a3c6120687265663d22687474703a2f2f6e67696e782e636f6d2f" \
+                                    "223e6e67696e782e636f6d3c2f613e2e3c2f703e0a0a3c703e3c656d3e54" \
+                                    "68616e6b20796f7520666f72207573696e67206e67696e782e3c2f656d3e" \
+                                    "3c2f703e0a3c2f626f64793e0a3c2f68746d6c3e0a"
+    return content_tls_appdata_all_plain
+
+
+@pytest.fixture
+def content_tls_appdata_all_cipher():
+    content_tls_appdata_all_cipher = "0193362a0c21d85d0e6b8b4152e68603df80bd0a4a3f5b657d88fdb100ba" \
+        "781703030020b8bbb51ff1f8fea5fb102bfecaa32d0a397e5365a00d117c" \
+        "b0c14cfa1f16018b170303001153deb3f534b6a08b3d0adb5a284bd5120c" \
+        "170303036bce38e1b771dbb8b5d70def1be64ac61d92d299a2909f353006" \
+        "28cccef3612ebdaa79f48713ee4835e8017175661c5af44497a65c15130f" \
+        "1ae6674ce274b49d14bb050a01c4a2ccdc4de05d18351cd458b3f3cc7f6d" \
+        "872ec170740ee599bba7aa5c6eaa5840f8dd82e4ccca29bae993ecc3a472" \
+        "0b4b66d13fbe1ab0a5f59ded31bfbcadbd8681177161710a62558e762bc0" \
+        "bd48ce3c12eb1aae11196bb351fa9ee6855ebf95b8770d7dcca7c687c83f" \
+        "1565f702478409799b6c6ba3dcd22028fe404d21e568f187d8f4f824e629" \
+        "7b8a1544f06fd2966e750e6feb40867fd66656e49aac5b7dbb17b3925e1a" \
+        "09a1551cea64ef9523ac76fc059b3203c894a156f9347aedc67a5abf880f" \
+        "b8e12add50242f359cdfcd5cbe304c24a7fb480a9ad5963767a16385dd2b" \
+        "ae363932a3495e9cd27ad272542683a6cf0933096f10b6281a17ef1fb1f8" \
+        "037d91799a08701a6e9cc6338e3baee8528a6f4b30542c46c2a44dde615b" \
+        "131abb448bb50a12af539e008b65164c56eb6a2751da9dd3d25a87bb17ab" \
+        "83b6f19df684a10281706a7abc87f5dcc2bbfb3848a409ea570b276610e5" \
+        "4c0d358136cb1e936ac24a2018252bd3beb8f8ef9035ef4e51e1867f21f2" \
+        "9fa5550a3db5cc16ea86c6d629b4442bb138a1e81aa6930e63dcb82626d6" \
+        "8050bf6805c32308d27cb7d3b5a0045029b47dbac9aeb38f0b2f26c4001d" \
+        "b15aa93ad79972d557ef4308692283fb976202667e056debbf75eb0d7a7c" \
+        "3c76edfd8e84c6e4800125184648d680e5ec59947daa18745a2f15281f98" \
+        "6483aed0c9b547df01325569c0850011a0f0154529985869de5e748bd460" \
+        "154bd014bf57503f336a3920c48453dd01d507868920656a54eb3c4aaf96" \
+        "beaf6a8f28bdc20523d7ed95612e9ab0c745b0aff3139c0a3da7962cce27" \
+        "73e7e55dc4638c4419d0332af45565289c7c9b751156328b3a49c59f0e46" \
+        "14d2ea460550f1865a41bbb70acab85292b2da21c813ff42906ec8147ba6" \
+        "473af102c212c2232574f51184989b0b6aad9e26137e07d85489780b5028" \
+        "006cc24663ebac1f753676a0c6fe934e8d2a8a03ff4d4ca3dfdc69978160" \
+        "bbeffe8b7c29f229942c7f3d572a49afbcb07efcac"
+    return content_tls_appdata_all_cipher
